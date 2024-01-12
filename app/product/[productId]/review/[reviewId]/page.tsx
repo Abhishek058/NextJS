d1fc0b6a69d0a52ef194ae4a1c639998164a9f09
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2024-01-12 13:19:49 +0530

    review not founf if id exced 1000

--- app/product/[productId]/review/[reviewId]/page.tsx
+++ app/product/[productId]/review/[reviewId]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 export default function page({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    return notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
